2aaeb79ca62f07c6b2bb84e9e9edf03d189f9b0e
czeslaw187 <czeslaw187@users.noreply.github.com>
2021-10-20 00:05:02 +0100

    db config

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php 
 
-$db_name = 'numerologia';
-$db_host = 'localhost';
-$db_username = 'root';
-$db_password = '';
+$db_name = 'opinions';
+$db_host = 'heroku_41af50f75eea690';
+$db_username = 'b4f7374db4f610';
+$db_password = 'b2f37a40';
 $db_port = 3306;
 
 ?>
