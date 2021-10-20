567668163609d17aa317e4ae7aec21274e130697
czeslaw187 <czeslaw187@users.noreply.github.com>
2021-10-20 02:21:32 +0100

    1

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php 
 
-$db_name = 'numerologia';
-$db_host = 'heroku_41af50f75eea690';
+$db_name = 'heroku_41af50f75eea690';
+$db_host = 'eu-cdbr-west-01.cleardb.com';
 $db_username = 'b4f7374db4f610';
 $db_password = 'b2f37a40';
 $db_port = 3306;
